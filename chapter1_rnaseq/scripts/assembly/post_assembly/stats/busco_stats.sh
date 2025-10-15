273efd4fc76d579ea7ec32b4698931630057ab09
Carlota Myhre De Gouveia <carlota@login-3.saga>
2025-10-15 10:40:21 +0200

    add busco script updated with threads option

--- chapter1_rnaseq/scripts/assembly/post_assembly/stats/busco_stats.sh
+++ chapter1_rnaseq/scripts/assembly/post_assembly/stats/busco_stats.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Run BUSCO on a Trinity-assembled transcriptome using the Saga module system.
-
 # Check arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <input_fasta> <lineage_dataset> <output_dir>"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <input_fasta> <lineage_dataset> <output_dir> [num_threads]"
     exit 1
 fi
 
 INPUT_FASTA="$1"
 LINEAGE="$2"
 OUTPUT_DIR="$3"
+THREADS="${4:-1}"  # Default to 1 thread if not provided
 
 # Load BUSCO module
 module load BUSCO/5.5.0-foss-2022b
@@ -21,9 +20,11 @@ mkdir -p "$OUTPUT_DIR"
 # Change to output directory to contain all BUSCO artifacts
 cd "$OUTPUT_DIR" || exit
 
-# Run BUSCO using relative path to input FASTA
+
+# Run BUSCO with specified number of threads
 busco -i "../../../../../$INPUT_FASTA" \
       -l "$LINEAGE" \
-      -o busco \
+      -o busco_run \
       -m transcriptome \
+      -c "$THREADS" \
       --out_path .
